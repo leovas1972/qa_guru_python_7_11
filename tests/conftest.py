1731c3ca10201ee139276b0b113a0328b0cc7f10
Vasilii Leontev <leovas1972@gmail.com>
2023-09-28 11:52:43 +0700

    created a conftest with a fixture

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selene.support.shared import browser
+
+
+@pytest.fixture(scope='function')
+def open_browser():
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
+    browser.config.set_value_by_js = True
+    browser.config.with_(click_by_js=True)
+
+    yield
+
+    browser.quit()
