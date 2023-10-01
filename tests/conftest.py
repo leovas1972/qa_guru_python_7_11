6c63f49703d5fcef0e31c9e2eae7cbcd52319349
Vasilii Leontev <leovas1972@gmail.com>
2023-10-01 07:05:43 +0700

    clousing the browser

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,14 +2,10 @@ import pytest
 from selene.support.shared import browser
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function", autouse=True)
 def open_browser():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
-    browser.config.set_value_by_js = True
-    browser.config.with_(click_by_js=True)
 
     yield
 
-    browser.close()
+    browser.quit()
